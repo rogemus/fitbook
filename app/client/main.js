a435ad17691e15c631583a84feb7d66074af2bda
Kacper Rogowski <Kacper Rogowski>
2016-09-02 07:47:23 +0200

    Aktualizacja paczek

--- app/client/main.js
+++ app/client/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class HelloWorld extends React.Component {
     render() {
-        return <h1>Helll;askd;salo!</h1>;
+        return <h1>Helll :) !</h1>;
     }
 }
 
