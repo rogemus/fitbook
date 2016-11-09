cad738bcbfd9448b9367dcd85db416d2659628dc
Michał <saidan95@gmail.com>
2016-11-09 14:47:52 +0100

    location search log,lng flip

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,8 +5,15 @@ class Location < ApplicationRecord
   class << self
 
     def fits_in_view(tl, br)
-      {:latitude => tl[:latitude]..br[:longitude],
-       :longitude => tl[:longitude]..br[:longitude]}
+      tl_lat = tl[:latitude]
+      tl_long = tl[:longitude]
+      br_lat = br[:latitude]
+      br_long = br[:longitude]
+
+      tl_lat, br_lat = br_lat, tl_lat if tl_lat > br_lat
+      tl_long, br_long = br_long, tl_long if tl_long > br_long
+
+      {:latitude => tl_lat..br_lat, :longitude => tl_long..br_long}
     end
 
   end
