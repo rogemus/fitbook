7ec640904300c58e6bc4c6da594855192c3c1586
Kacper Rogowski <rogemus@gmail.com>
2016-12-19 23:04:15 +0100

    poprawienie build proda

---
+++ webpack.prod.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'./src/index.js'
+	],
+	output: {
+		path: __dirname,
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				exclude: /node_modules/,
+				loader: 'babel'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+	]
+};
