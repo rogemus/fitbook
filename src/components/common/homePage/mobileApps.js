645b21a9fae2e08a319f5634ffce95dc2cb799e5
Kacper Rogowski <rogemus@gmail.com>
2017-01-07 19:01:11 +0100

    update

--- src/components/common/homePage/mobileApps.js
+++ src/components/common/homePage/mobileApps.js
@@ -6,15 +6,23 @@ export default () => {
 			<div className="mobile-apps-wrapper">
 				<div className="row">
 					<div className="col col-2-5">
-						<div className="mobile-apps-title"><h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1></div>
-						<div className="mobile-apps-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
-							aliquam aperiam atque consequatur delectus dignissimos earum eius esse in iure molestiae, nostrum
-							nulla obcaecati perferendis quae velit, voluptatum. Ad, porro?
+						<div className="mobile-apps-title"><h1>Download our app</h1></div>
+						<div className="mobile-apps-content">
+							<div className="apps">
+								<a href="">
+									<img src="/img/win.png" alt=""/>
+								</a>
+							</div>
+							<div className="apps">
+								<a href="">
+									<img src="/img/and.png" alt=""/>
+								</a>
+							</div>
 						</div>
 					</div>
 					<div className="col col-3-5">
 						<div className="mobile-apps-showcase">
-							<img src="/img/macbook.png" alt=""/>
+							<img src="/img/apps.png" alt=""/>
 						</div>
 					</div>
 				</div>
