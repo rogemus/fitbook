aaa0c146425db687fda18df74b87db230bd7aa0c
Kacper Rogowski <rogemus@gmail.com>
2017-01-08 16:48:19 +0100

    fix

--- src/components/common/ratingSection.js
+++ src/components/common/ratingSection.js
@@ -23,34 +23,38 @@ class RatingSection extends React.Component {
 	}
 
 	renderRating() {
-		if (this.props.current_user) {
-			return (
-				<section className="rating">
-					<div className="rating-wrapper">
-						<div className="row">
-							<div className="col col-1-2">
-								<div className="rating-title">
-									<h1>Rated by <span className="rating-counter">{this.props.count}</span> people</h1>
+		if (this.props.authenticated) {
+			if (this.props.current_user) {
+				return (
+					<section className="rating">
+						<div className="rating-wrapper">
+							<div className="row">
+								<div className="col col-1-2">
+									<div className="rating-title">
+										<h1>Rated by <span className="rating-counter">{this.props.count}</span> people
+										</h1>
+									</div>
+								</div>
+								<div className="col col-1-2">
+									<form ref="formRating" name="formRating" id="formRating"
+										  className="formRating rating-form" onSubmit={this.handleSelectRating}>
+										<label htmlFor="ratingSelect">Pick your rating</label>
+										<select name="ratingSelect" id="ratingSelect"
+												onChange={this.handleSelectRating}>
+											<option value="Choose here" hidden>Choose here</option>
+											<option value="5">5</option>
+											<option value="4">4</option>
+											<option value="3">3</option>
+											<option value="2">2</option>
+											<option value="1">1</option>
+										</select>
+									</form>
 								</div>
 							</div>
-							<div className="col col-1-2">
-								<form ref="formRating" name="formRating" id="formRating"
-									  className="formRating rating-form" onSubmit={this.handleSelectRating}>
-									<label htmlFor="ratingSelect">Pick your rating</label>
-									<select name="ratingSelect" id="ratingSelect" onChange={this.handleSelectRating}>
-										<option value="Choose here" hidden>Choose here</option>
-										<option value="5">5</option>
-										<option value="4">4</option>
-										<option value="3">3</option>
-										<option value="2">2</option>
-										<option value="1">1</option>
-									</select>
-								</form>
-							</div>
 						</div>
-					</div>
-				</section>
-			);
+					</section>
+				);
+			}
 		}
 	}
 
@@ -63,7 +67,8 @@ class RatingSection extends React.Component {
 
 function mapStateToProps(state) {
 	return {
-		current_user: state.current_user.user
+		current_user: state.current_user.user,
+		authenticated: state.auth.authenticated
 	};
 }
 
